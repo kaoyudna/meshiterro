4fd968f44f2eac9eb38297fdf8f14e6c5f54dd23
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-01-09 23:37:58 +0000

    [Add]5-2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.active_job.queue_adapter = :inline
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -73,4 +75,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts.clear
 end
